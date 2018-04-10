4488f8c686b013c3ebf0f31eeef8d6d150a298c5
Roger <rooger.hermann@gmail.com>
2018-04-09 23:00:09 -0300

    Ajuste contato

--- contato.php
+++ contato.php
@@ -1,3 +1,3 @@
 <h2>Página de contato</h2>
 
-<p>Deixe sua mensagem e entraremos em contato assim que possível.</p>
+<p>Deixe sua mensagem e entraremos em contato assim que possível. Muito obrigado.</p>
